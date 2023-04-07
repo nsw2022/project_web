0735b1f86623861574ceaa89f02f0ac37e2099fe
nsw2022 <tmddn3410@naver.com>
2023-04-07 17:41:06 +0900

    04_07_animation_cafe

---
+++ cafe12/js/main-js.js
@@ -0,0 +1,26 @@
+window.onload = function(){
+
+    // 디지털 시계
+    setInterval(setWatch,1000) // 함수 호출
+
+    function setWatch(){
+        const date = new Date()
+        let now = date.toLocaleTimeString() // 시간형식
+        document.getElementById("demo").innerHTML = now
+    }
+
+    // 배경 이미지 change
+    let picture = ["./images/header1.jpg","./images/header2.jpg","./images/header3.jpg"]
+    let imgIdx = 0
+    
+    showPicture() // 함수호출
+    
+    function showPicture(){    
+        let img = document.getElementById('pic')
+        img.src = picture[imgIdx]
+        imgIdx++
+        if(imgIdx==picture.length) imgIdx=0  
+        console.log(imgIdx)
+        setTimeout(showPicture,2000) // 콜백 함수
+    }
+}
